8ef20c01644849dfe11dc0b9d76ea7de26ead0f9
Greg Groves <136647013+ggrovesah@users.noreply.github.com>
2023-06-16 18:37:02 -0400

    Update streamlit_app.py
    Further breakfast menu changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("KAle, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🍲 Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥬 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑Avocado Toast")
