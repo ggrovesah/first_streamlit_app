2387ca0a0c639e71db9187c6e685fcc91cb53457
Greg Groves <136647013+ggrovesah@users.noreply.github.com>
2023-07-19 18:08:12 -0400

    add user entry of fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+#New Section to display fruityvice api response
+streamlit.header ('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+ 
+import requests
+
+fruityvice response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
